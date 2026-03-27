5129c8eca9cc581f868bf2bc5d0186ad0851647c
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-27 20:02:43 +0300

    feat: add the class ingredient

--- src/main/java/org/example/ingredientspring/entity/Ingredient.java
+++ src/main/java/org/example/ingredientspring/entity/Ingredient.java
@@ -1,4 +1,27 @@
 package org.example.ingredientspring.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+    private Double price;
+    
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum category;
+    
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_ingredient")
+    private List<StockMovement> stockMovementList;
 }
