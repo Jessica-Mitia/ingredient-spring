8bdbbcc48d03ad625cac92e5bca5268c014c8266
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-27 20:03:32 +0300

    feat: add the class stockValue

--- src/main/java/org/example/ingredientspring/entity/StockValue.java
+++ src/main/java/org/example/ingredientspring/entity/StockValue.java
@@ -1,4 +1,16 @@
 package org.example.ingredientspring.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
 public class StockValue {
+    private double quantity;
+    
+    @Enumerated(EnumType.STRING)
+    private UnitTypeEnum unit;
 }
