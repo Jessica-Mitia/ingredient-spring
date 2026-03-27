100e077a3f8ff7d7d4a5c5dc082fc44fafde52e3
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-27 20:01:43 +0300

    feat: add the class dish

--- src/main/java/org/example/ingredientspring/entity/Dish.java
+++ src/main/java/org/example/ingredientspring/entity/Dish.java
@@ -1,4 +1,27 @@
 package org.example.ingredientspring.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Dish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+    
+    @Enumerated(EnumType.STRING)
+    private DishTypeEnum dishType;
+    
+    @OneToMany(mappedBy = "dish", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DishIngredient> dishIngredients;
+    
+    private Double price;
 }
