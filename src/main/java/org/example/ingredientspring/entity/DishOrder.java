177a273124febb0edb7b8e5de5fc22504d040b73
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-27 20:18:14 +0300

    feat: add the class DishOrder

--- src/main/java/org/example/ingredientspring/entity/DishOrder.java
+++ src/main/java/org/example/ingredientspring/entity/DishOrder.java
@@ -1,4 +1,28 @@
 package org.example.ingredientspring.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class DishOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_dish")
+    private Dish dish;
+
+    @ManyToOne
+    @JoinColumn(name = "id_order")
+    private Order order;
+
+    private Integer quantity;
 }
