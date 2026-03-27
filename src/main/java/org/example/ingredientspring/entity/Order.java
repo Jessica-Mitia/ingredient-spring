0c39cc74c808633b86b1480eb9d4e9e9e660fc1d
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-27 20:18:29 +0300

    feat: add the class Order

--- src/main/java/org/example/ingredientspring/entity/Order.java
+++ src/main/java/org/example/ingredientspring/entity/Order.java
@@ -1,4 +1,29 @@
 package org.example.ingredientspring.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String reference;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Instant creationDateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DishOrder> dishOrders;
 }
