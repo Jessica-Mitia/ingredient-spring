c3d5360d2b062f61574602ec8efe65f52fb228d0
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-30 11:46:10 +0300

    update Dishcontroller

--- src/main/java/org/example/ingredientspring/controller/DishController.java
+++ src/main/java/org/example/ingredientspring/controller/DishController.java
@@ -1,4 +1,34 @@
 package org.example.ingredientspring.controller;
 
+import org.example.ingredientspring.dto.DishResponse;
+import org.example.ingredientspring.entity.Ingredient;
+import org.example.ingredientspring.exception.BadRequestException;
+import org.example.ingredientspring.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/dishes")
 public class DishController {
+
+    @Autowired
+    private DishService dishService;
+
+    @GetMapping
+    public List<DishResponse> getAllDishes() {
+        return dishService.findAll().stream()
+                .map(DishResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping("/{id}/ingredients")
+    public List<Ingredient> updateDishIngredients(@PathVariable Integer id, @RequestBody(required = false) List<Ingredient> ingredients) {
+        if (ingredients == null) {
+            throw new BadRequestException("Le corps de la requête ne peut pas être vide.");
+        }
+        return dishService.updateDishIngredients(id, ingredients);
+    }
 }
