b08f3f4cfa5f6ff7f7beb4cc22da3fd98f69f8a8
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-30 11:42:32 +0300

    feat: add the ingredient repository

--- src/main/java/org/example/ingredientspring/repository/IngredientRepository.java
+++ src/main/java/org/example/ingredientspring/repository/IngredientRepository.java
@@ -1,4 +1,9 @@
 package org.example.ingredientspring.repository;
 
-public class IngredientRepository {
+import org.example.ingredientspring.entity.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 }
